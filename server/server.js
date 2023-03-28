f4f6019064c9a86e72b35890e4febf6fd73632ab
bestw07 <devspeed.vision.it@gmail.com>
2023-03-28 20:54:23 +0200

    fix deploy on heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const {
   ApolloServer,
-
-  // PubSub
 } = require("apollo-server");
 const mongoose = require("mongoose");
 
@@ -11,15 +9,14 @@ const { PubSub } = require("graphql-subscriptions");
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-// const typeDefs = require("./graphql/typeDefs");
-// const resolvers = require("./graphql/resolvers");
-// const { MONGODB } = require("./config.js");
 const { connection } = require("./config/connection");
 
 const pubsub = new PubSub();
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.Port_ApolloServer || 3001;
 
 const server = new ApolloServer({
   typeDefs,
@@ -27,24 +24,25 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubsub }),
 });
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static('../client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../client/build/index.html'));
+app.use(bodyParser.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+
+  app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
+  console.log('production mode ===');
 } else {
   app.get('*', (req, res) => {
     res.status(200).json({ message: 'welcome server...' })
   })
 }
 
-app.listen(process.env.port || 3002, '0.0.0.0', () => {
-  console.log('node app is running on');
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+  console.log('node app is running ===');
 })
-// mongoose
-//   .connect(MONGODB, { useNewUrlParser: true })
-// connection
-//   .once("open")
 
 mongoose
   .connect("mongodb+srv://subhan:subhan@cluster0.pyiicf8.mongodb.net/gym", {
@@ -52,20 +50,9 @@ mongoose
   })
   .then(() => {
     // .then(() => {
-    console.log("MongoDB Connected");
+    console.log("MongoDB Connected ===");
     return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
   });
-
-  // app.listen(PORT, () => {
-  //   console.log('server is running on', PORT);
-  // })
-
-// .then((res) => {
-//   console.log(`Server running at ${res.url}`);
-// })
-// .catch((err) => {
-//   console.error(err);
-// });
